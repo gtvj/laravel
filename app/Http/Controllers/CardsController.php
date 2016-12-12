0d213d1b6d3b96f48fa6fc8f38496aa040efc0a5
Gwyn Jones <m@gwnjns.com>
2016-12-12 22:52:05 +0000

    Lesson 7: Fetching Data (Part 2)
    Demonstrates how 'convention' can be used to leverage Laravel's
    'route model binding'.

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -15,9 +15,8 @@ class CardsController extends Controller
         return view('cards.index', compact('cards'));
     }
 
-    public function show($id)
+    public function show(Card $card)
     {
-        $card =  Card::find($id);
         return view('cards.show', compact('card'));
     }
 }
